b74cd03df441b81f4ba987673cf4bb59ce757aa5
mwendeanncasty <122888716+mwendeanncasty@users.noreply.github.com>
2023-01-17 20:50:45 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+#This in my first code
+"""I am very pleased for learning the new programming language and it is very intreseting
+"""
+print("Anncasty Mwende")
+print("This is my first code in python")
+print(True)
+a = 56.4
+print(a)
+name = "Python"
+b = 20
+print(b)
+print(name)
+
